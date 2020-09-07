3f61df23036f461ce5e885e958888c3f83ab8fe5
Raynald-su <raynald.shu@gmail.com>
2020-09-07 16:37:01 +0700

    Initial Commit for Bake CakePhp

---
+++ Config/database.php
@@ -0,0 +1,20 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'trainingcakephp',
+	);
+	public $trainingcakephp = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'trainingcakephp',
+	);
+}
